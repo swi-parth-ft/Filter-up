fb8e84efc3ed0410fd29e0e5ed55d0095de4cc94
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-14 10:34:57 -0400

    Initial Commit

---
+++ Filter up/Filter_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  Filter_upApp.swift
+//  Filter up
+//
+//  Created by Parth Antala on 2024-07-14.
+//
+
+import SwiftUI
+
+@main
+struct Filter_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
